bfbe5d19a82dc26eda4f0fbd5e4fe139520a0a93
qinzj <qzjian@gmail.com>
2019-05-26 19:35:13 +0800

    Open permissions

--- BBQ/UIView+layout.swift
+++ BBQ/UIView+layout.swift
@@ -1,6 +1,6 @@
 //
 //  UIView+layout.swift
-//  AutoLayoutDemo
+//  BBQKit
 //
 //  Created by qzjian on 2019/5/25.
 //  Copyright © 2019年 qzjian. All rights reserved.
@@ -11,54 +11,54 @@ import UIKit
 extension UIView {
 
     @discardableResult
-    func centerX(_ offset: CGFloat = 0, _ targetV: UIView? = nil) -> UIView {
+    public func centerX(_ offset: CGFloat = 0, _ targetV: UIView? = nil) -> UIView {
         let target = targetV == nil ? self.superview! : targetV!
         self.centerXAnchor.constraint(equalTo: target.centerXAnchor, constant: offset).isActive = true
         return self
     }
 
     @discardableResult
-    func centerY(_ offset: CGFloat = 0, _ targetV: UIView? = nil) -> UIView {
+    public func centerY(_ offset: CGFloat = 0, _ targetV: UIView? = nil) -> UIView {
         let target = targetV == nil ? self.superview! : targetV!
         self.centerYAnchor.constraint(equalTo: target.centerYAnchor, constant: offset).isActive = true
         return self
     }
 
     @discardableResult
-    func width(_ width: CGFloat = 0) -> UIView {
+    public func width(_ width: CGFloat = 0) -> UIView {
         self.widthAnchor.constraint(equalToConstant: width).isActive = true
         return self
     }
 
     @discardableResult
-    func height(_ height: CGFloat = 0) -> UIView {
+    public func height(_ height: CGFloat = 0) -> UIView {
         self.heightAnchor.constraint(equalToConstant: height).isActive = true
         return self
     }
 
     @discardableResult
-    func widthEqualTo(_ targetV: UIView? = nil, _ multi: CGFloat = 1, _ offset: CGFloat = 0) -> UIView {
+    public func widthEqualTo(_ targetV: UIView? = nil, _ multi: CGFloat = 1, _ offset: CGFloat = 0) -> UIView {
         let target = targetV == nil ? self.superview! : targetV!
         self.widthAnchor.constraint(equalTo: target.widthAnchor, multiplier: multi, constant: offset).isActive = true
         return self
     }
 
     @discardableResult
-    func heightEqualTo(_ targetV: UIView? = nil, _ multi: CGFloat = 1, _ offset: CGFloat = 0) -> UIView {
+    public func heightEqualTo(_ targetV: UIView? = nil, _ multi: CGFloat = 1, _ offset: CGFloat = 0) -> UIView {
         let target = targetV == nil ? self.superview! : targetV!
         self.heightAnchor.constraint(equalTo: target.heightAnchor, multiplier: multi, constant: offset).isActive = true
         return self
     }
 
     @discardableResult
-    func size(_ width: CGFloat = 0, _ height: CGFloat = 0) -> UIView {
+    public func size(_ width: CGFloat = 0, _ height: CGFloat = 0) -> UIView {
         NSLayoutConstraint.activate([self.widthAnchor.constraint(equalToConstant: width),
-                                    self.heightAnchor.constraint(equalToConstant: height)])
+                                     self.heightAnchor.constraint(equalToConstant: height)])
         return self
     }
 
     @discardableResult
-    func bottom(_ offset: CGFloat = 0, _ targetV: UIView? = nil, _ reverse: Bool = false) -> UIView {
+    public func bottom(_ offset: CGFloat = 0, _ targetV: UIView? = nil, _ reverse: Bool = false) -> UIView {
         let target = targetV == nil ? self.superview! : targetV!
         let anchor = reverse ? target.topAnchor : target.bottomAnchor
         self.bottomAnchor.constraint(equalTo: anchor, constant: offset).isActive = true
@@ -66,7 +66,7 @@ extension UIView {
     }
 
     @discardableResult
-    func top(_ offset: CGFloat = 0, _ targetV: UIView? = nil, _ reverse: Bool = false) -> UIView {
+    public func top(_ offset: CGFloat = 0, _ targetV: UIView? = nil, _ reverse: Bool = false) -> UIView {
         let target = targetV == nil ? self.superview! : targetV!
         let anchor = reverse ? target.bottomAnchor : target.topAnchor
         self.topAnchor.constraint(equalTo: anchor, constant: offset).isActive = true
@@ -74,7 +74,7 @@ extension UIView {
     }
 
     @discardableResult
-    func left(_ offset: CGFloat = 0, _ targetV: UIView? = nil, _ reverse: Bool = false) -> UIView {
+    public func left(_ offset: CGFloat = 0, _ targetV: UIView? = nil, _ reverse: Bool = false) -> UIView {
         let target = targetV == nil ? self.superview! : targetV!
         let anchor = reverse ? target.rightAnchor : target.leftAnchor
         self.leftAnchor.constraint(equalTo: anchor, constant: offset).isActive = true
@@ -82,7 +82,7 @@ extension UIView {
     }
 
     @discardableResult
-    func right(_ offset: CGFloat = 0, _ targetV: UIView? = nil, _ reverse: Bool = false) -> UIView {
+    public func right(_ offset: CGFloat = 0, _ targetV: UIView? = nil, _ reverse: Bool = false) -> UIView {
         let target = targetV == nil ? self.superview! : targetV!
         let anchor = reverse ? target.leftAnchor : target.rightAnchor
         self.rightAnchor.constraint(equalTo: anchor, constant: offset).isActive = true
@@ -90,7 +90,7 @@ extension UIView {
     }
 
     @discardableResult
-    func leading(_ offset: CGFloat = 0, _ targetV: UIView? = nil, _ reverse: Bool = false) -> UIView {
+    public func leading(_ offset: CGFloat = 0, _ targetV: UIView? = nil, _ reverse: Bool = false) -> UIView {
         let target = targetV == nil ? self.superview! : targetV!
         let anchor = reverse ? target.trailingAnchor : target.leadingAnchor
         self.leadingAnchor.constraint(equalTo: anchor, constant: offset).isActive = true
@@ -98,7 +98,7 @@ extension UIView {
     }
 
     @discardableResult
-    func trailing(_ offset: CGFloat = 0, _ targetV: UIView? = nil, _ reverse: Bool = false) -> UIView {
+    public func trailing(_ offset: CGFloat = 0, _ targetV: UIView? = nil, _ reverse: Bool = false) -> UIView {
         let target = targetV == nil ? self.superview! : targetV!
         let anchor = reverse ? target.leadingAnchor : target.trailingAnchor
         self.trailingAnchor.constraint(equalTo: anchor, constant: offset).isActive = true
@@ -106,7 +106,7 @@ extension UIView {
     }
 
     @discardableResult
-    func bbq() -> UIView? {
+    public func bbq() -> UIView? {
         guard self.superview != nil else {
             print("please insure already added to a parentview")
             return nil
