828c58aaad66446276ff6bc28990b9401e10bbf3
qinzj <qzjian@gmail.com>
2019-06-21 20:48:29 +0800

    rename

--- BBQ/TableProxy/BBQSectionTableViewProxy.swift
+++ BBQ/TableProxy/BBQSectionTableViewProxy.swift
@@ -10,27 +10,27 @@ import UIKit
 
 public class BBQSectionTableViewProxy: NSObject, UITableViewDataSource, UITableViewDelegate {
 
-    private var dataSources: [BBQTableViewProxy<Any>]
+    private var tableProxys: [BBQTableViewProxy<Any>]
 
-    public init(dataSources: [BBQTableViewProxy<Any>]) {
-        self.dataSources = dataSources
+    public init(tableProxys: [BBQTableViewProxy<Any>]) {
+        self.tableProxys = tableProxys
     }
 
     public func numberOfSections(in tableView: UITableView) -> Int {
-        return dataSources.count
+        return tableProxys.count
     }
 
     public func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        let dataSource = dataSources[section]
-        return dataSource.tableView(tableView, numberOfRowsInSection: 0)
+        let proxy = tableProxys[section]
+        return proxy.tableView(tableView, numberOfRowsInSection: 0)
     }
 
     public func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let dataSource = dataSources[indexPath.section]
+        let proxy = tableProxys[indexPath.section]
         let indexPath = IndexPath(row: indexPath.row, section: 0)
-        return dataSource.tableView(tableView, cellForRowAt: indexPath)
+        return proxy.tableView(tableView, cellForRowAt: indexPath)
     }
 }
 
@@ -38,19 +38,19 @@ extension BBQSectionTableViewProxy {
 
     // Editing
     public func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        let dataSource = dataSources[indexPath.section]
+        let proxy = tableProxys[indexPath.section]
         let indexPath = IndexPath(row: indexPath.row, section: 0)
-        return dataSource.tableView(tableView, canEditRowAt: indexPath)
+        return proxy.tableView(tableView, canEditRowAt: indexPath)
     }
 
     public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
 
         if editingStyle == .delete {
-            var tmpDataSources = dataSources
-            let dataSource = tmpDataSources[indexPath.section]
+            var tmptableProxys = tableProxys
+            let dataSource = tmptableProxys[indexPath.section]
             dataSource.remove(at: indexPath.row)
-            tmpDataSources[indexPath.section] = dataSource
-            dataSources = tmpDataSources
+            tmptableProxys[indexPath.section] = dataSource
+            tableProxys = tmptableProxys
             tableView.beginUpdates()
             tableView.deleteRows(at: [indexPath], with: .automatic)
             tableView.endUpdates()
@@ -58,56 +58,56 @@ extension BBQSectionTableViewProxy {
     }
 
     public func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        let dataSource = dataSources[indexPath.section]
+        let proxy = tableProxys[indexPath.section]
         let indexPath = IndexPath(row: indexPath.row, section: 0)
-        return dataSource.tableView(tableView, canMoveRowAt: indexPath)
+        return proxy.tableView(tableView, canMoveRowAt: indexPath)
     }
 
     public func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
 
-        let srcDataSource = dataSources[sourceIndexPath.section]
-        let dstDataSource = dataSources[destinationIndexPath.section]
+        let srcProxy = tableProxys[sourceIndexPath.section]
+        let dstProxy = tableProxys[destinationIndexPath.section]
         if sourceIndexPath.section == destinationIndexPath.section {
-            srcDataSource.tableView(tableView, moveRowAt: sourceIndexPath, to: destinationIndexPath)
-            dataSources[sourceIndexPath.section] = srcDataSource
+            srcProxy.tableView(tableView, moveRowAt: sourceIndexPath, to: destinationIndexPath)
+            tableProxys[sourceIndexPath.section] = srcProxy
             return 
         }
 
-        guard let model = srcDataSource.model(at: sourceIndexPath.row) else { return }
+        guard let model = srcProxy.model(at: sourceIndexPath.row) else { return }
 
-        srcDataSource.remove(at: sourceIndexPath.row)
-        dataSources[sourceIndexPath.section] = srcDataSource
+        srcProxy.remove(at: sourceIndexPath.row)
+        tableProxys[sourceIndexPath.section] = srcProxy
 
-        dstDataSource.insert(model, at: destinationIndexPath.row)
-        dataSources[destinationIndexPath.section] = dstDataSource
+        dstProxy.insert(model, at: destinationIndexPath.row)
+        tableProxys[destinationIndexPath.section] = dstProxy
         tableView.reloadData()
     }
 
     // MARK: - config header and footer
     public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let dataSource = dataSources[section]
-        return dataSource.tableView(tableView, viewForHeaderInSection: 0)
+        let proxy = tableProxys[section]
+        return proxy.tableView(tableView, viewForHeaderInSection: 0)
     }
 
     public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let dataSource = dataSources[section]
-        return dataSource.tableView(tableView, viewForFooterInSection: 0)
+        let proxy = tableProxys[section]
+        return proxy.tableView(tableView, viewForFooterInSection: 0)
     }
 
     public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        let dataSource = dataSources[section]
-        return dataSource.tableView(tableView, heightForHeaderInSection: section)
+        let proxy = tableProxys[section]
+        return proxy.tableView(tableView, heightForHeaderInSection: section)
     }
 
     public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        let dataSource = dataSources[section]
-        return dataSource.tableView(tableView, heightForFooterInSection: section)
+        let proxy = tableProxys[section]
+        return proxy.tableView(tableView, heightForFooterInSection: section)
     }
 
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let dataSource = dataSources[indexPath.section]
+        let proxy = tableProxys[indexPath.section]
         let indexPath = IndexPath(row: indexPath.row, section: 0)
-        dataSource.tableView(tableView, didSelectRowAt: indexPath)
+        proxy.tableView(tableView, didSelectRowAt: indexPath)
         tableView.deselectRow(at: tableView.indexPathForSelectedRow ?? indexPath, animated: true)
     }
 }
